269b25ebd5377da4440680f73453fe6df8ad41f6
Evan Scott <evan.scott@zocdoc.com>
2018-05-02 14:59:09 -0400

    add es6 export

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-"use strict";
-
 var tester = /^[-!#$%&'*+\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-?\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
 // Thanks to:
 // http://fightingforalostcause.net/misc/2006/compare-email-regex.php
 // http://thedailywtf.com/Articles/Validating_Email_Addresses.aspx
 // http://stackoverflow.com/questions/201323/what-is-the-best-regular-expression-for-validating-email-addresses/201378#201378
-exports.validate = function(email)
+export function validate(email)
 {
 	if (!email)
 		return false;
-		
+
 	if(email.length>254)
 		return false;
 
